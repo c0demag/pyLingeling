f98dc70c5965d554b84d5639961ec8630f99e93e
Pramod Subramanyan <pramod@berkeley.edu>
2018-09-07 20:10:51 +0530

    updated demo.

--- demo.py
+++ demo.py
@@ -1,16 +1,9 @@
-import picosat
+import lingeling
 
-with open("/tmp/log", "w") as fout:
-    p = picosat.picosat_init()
-    picosat.picosat_set_verbosity(p, 100)
-    f = picosat.picosat_set_output(p, fout)
-    picosat.picosat_measure_all_calls(p)
-    picosat.picosat_inc_max_var(p)
-    picosat.picosat_add(p, 1)
-    picosat.picosat_add(p, -1)
-    picosat.picosat_add(p, 0)
-    assert picosat.picosat_sat(p, -1) == picosat.PICOSAT_SATISFIABLE
-    picosat.picosat_message(p, 0, "End")
-    picosat.picosat_flushout(f)
-    picosat.picosat_reset(p)
+p = lingeling.lglinit()
+lingeling.lgladd(p, 1)
+lingeling.lgladd(p, -1)
+lingeling.lgladd(p, 0)
+lingeling.lglassume(p, -1)
+assert lingeling.lglsat(p) == lingeling.LGL_SATISFIABLE
 print("Done.")
