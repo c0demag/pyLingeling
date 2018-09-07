3b58151b06ce493fc94416c53249afd0ca415130
Pramod Subramanyan <pramod@berkeley.edu>
2018-09-07 19:59:06 +0530

    Converting to Lingeling.

--- build.sh
+++ build.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-PICOSAT_DIR=$1
+LINGELING_DIR=lingeling
 PYTHON=`which python`
 #PYTHON=`which python3.5`
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if [ "X${PICOSAT_DIR}" == "X" ]; then
-    echo "Usage: ./build.sh <picosat_dir>"
-    exit 1
-fi
-
-cd $PICOSAT_DIR
+cd $LINGELING_DIR
 export CFLAGS=" -fPIC"
 ./configure.sh -O
 make
 
 cd $DIR
 # SWIG (swig is handled within setup.py)
-# swig -I${PICOSAT_DIR} -python -o picosat_python_wrap.c picosat_python.i
+# swig -I${LINGELING_DIR} -python -o lingeling_python_wrap.c lingeling_python.i
 # Build
 $PYTHON ./setup.py build
 
--- setup.py
+++ setup.py
@@ -3,49 +3,49 @@ from setuptools import setup
 from distutils.extension import Extension
 from datetime import datetime
 
-PICOSAT_VERSION='965'
-PICOSAT_DIR='picosat-%s' % PICOSAT_VERSION
+LINGELING_VERSION='160707'
+LINGELING_DIR='lingeling'
 
-PYPICOSAT_MINOR_VERSION='%s' % datetime.utcnow().strftime("%y%m%d%H%M")
+PYLINGELING_MINOR_VERSION='%s' % datetime.utcnow().strftime("%y%m%d%H%M")
 # Major number is Picosat Version, minor number creation date of the bindings
-PYPICOSAT_VERSION='%s.%s' % (PICOSAT_VERSION, PYPICOSAT_MINOR_VERSION)
+PYLINGELING_VERSION='%s.%s' % (LINGELING_VERSION, PYLINGELING_MINOR_VERSION)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Build from SWIG definition
-picosat_ext = Extension('_picosat',
-                        [os.path.join(BASE_DIR, 'picosat_python.i')],
-                        swig_opts=['-I%s' % os.path.join(BASE_DIR, PICOSAT_DIR)],
-                        include_dirs=[PICOSAT_DIR],
-                        library_dirs=[PICOSAT_DIR],
-                        libraries=['picosat'],
+lingeling_ext = Extension('_lingeling',
+                        [os.path.join(BASE_DIR, 'lingeling_python.i')],
+                        swig_opts=['-I%s' % os.path.join(BASE_DIR, LINGELING_DIR)],
+                        include_dirs=[LINGELING_DIR],
+                        library_dirs=[LINGELING_DIR],
+                        libraries=['lgl'],
                         language='c',
                     )
 
 
-short_description="Picosat SAT-Solver Wrapper"
+short_description="Lingeling SAT-Solver Wrapper"
 long_description=\
 """
-==========================
-Picosat SAT-Solver Wrapper
-==========================
+============================
+Lingeling SAT-Solver Wrapper
+============================
 
-pyPicosat provides a basic wrapping around the efficient Picosat SAT-Solver.
+pyLingeling provides a basic wrapping around the efficient Lingeling SAT-Solver.
 
 To build this wrapper you need SWIG to be installed.
 
-Picosat is developed by Armin Biere, for more information: http://fmv.jku.at/picosat/
+Lingeling is developed by Armin Biere, for more information: http://fmv.jku.at/lingeling/
 """
 
-setup(name='pyPicosat',
-      version=PYPICOSAT_VERSION,
-      author='PySMT Team',
-      author_email='info@pysmt.org',
-      url='https://github.com/pysmt/pyPicoSAT/',
+setup(name='pyLingeling',
+      version=PYLINGELING_VERSION,
+      author='Pramod Subramanyan',
+      author_email='pramod.subramanyan@gmail.com',
+      url='https://github.com/pramodsu/pyLingeling/',
       license='BSD',
       description=short_description,
       long_description=long_description,
-      ext_modules=[picosat_ext],
-      py_modules=['picosat'],
+      ext_modules=[lingeling_ext],
+      py_modules=['lingeling'],
       classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
